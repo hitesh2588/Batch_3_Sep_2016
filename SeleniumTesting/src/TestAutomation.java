b2eddf88723af44ca38c58d55fd8acf7a568d17d
Hitesh Pande <hitesh.pande1988@gmail.com>
2016-09-13 09:46:53 +0530

    update

--- SeleniumTesting/src/TestAutomation.java
+++ SeleniumTesting/src/TestAutomation.java
@@ -10,7 +10,7 @@ public class TestAutomation {
 
 	public static void main(String[] args) {
 
-		DefaultSelenium selenium = new DefaultSelenium("localhost", 4444, "firefox C:/Program Files (x86)/Mozilla Firefox/firefox.exe", "http://gmail.com");
+		DefaultSelenium selenium = new DefaultSelenium("localhost", 4444, "firefox C:/Program Files/Mozilla Firefox/firefox.exe", "http://gmail.com");
 		selenium.start();
 		selenium.open("/");
 		selenium.windowMaximize();
