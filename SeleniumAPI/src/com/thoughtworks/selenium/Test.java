079d7ef133b20473eae9127c648df1e96a432b2d
Hitesh Pande <hitesh.pande1988@gmail.com>
2016-09-18 15:33:22 +0530

    Project update

---
+++ SeleniumAPI/src/com/thoughtworks/selenium/Test.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.selenium;
+
+public class Test {
+	static int i;
+	static float f=10.5f;
+	static long l=12345678912l;
+	static boolean b;
+	static char c;
+	public static void main(String[] args) {
+		
+		//Test t1,t2= new Test();
+		
+		//System.out.println(t1);
+		System.out.println(i);
+		System.out.println(f);
+		System.out.println(l);
+		System.out.println(b);
+		System.out.println(c);
+
+	}
+
+}
--- SeleniumTesting/src/TestAutomation.java
+++ SeleniumTesting/src/TestAutomation.java
@@ -1,14 +1,14 @@
 import com.thoughtworks.selenium.DefaultSelenium;
 
+
 /**
  * @author Hitesh
  *
  */
-
-
 public class TestAutomation {
 
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws InterruptedException {
 
 		DefaultSelenium selenium = new DefaultSelenium("localhost", 4444, "firefox C:/Program Files/Mozilla Firefox/firefox.exe", "http://gmail.com");
 		selenium.start();
@@ -29,11 +29,16 @@ public class TestAutomation {
 			selenium.captureEntirePageScreenshot("D:/Screenshot/EntirePageError.jpg", "background=#1A55D6");
 		}
 		
+		selenium.type("//*[@id='Email']", "hitesh.cool88@gmail.com");
 		selenium.click("next");
-		
-		/* selenium.close(); //Close Current Browser on which the main focus or site was opened.
-		selenium.stop(); // Kill Session - Close all related windows like tabs and popups*/
-		
+		Thread.sleep(2000);
+		selenium.type("//*[@id='Passwd']","qwerty");	
+		selenium.click("//*[@id='signIn']");
+		selenium.waitForPageToLoad("20000");
+		System.out.println(selenium.getText("//*[@id='errormsg_0_Passwd']"));
+				
+		/*selenium.close(); //Close Current Browser on which the main focus or site was opened.
+		selenium.stop(); // Kill Session - Close all related windows like tabs and popups*/		
 	}
 
 }
